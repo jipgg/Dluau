d405fba42aa31826865a9d36620f3c89d76f4f3e
Jip <83951137+jipgg@users.noreply.github.com>
2025-02-07 17:40:06 +0100

    cinterop api changes

--- src/libdlimport/cinterop/cinterop.cpp
+++ src/libdlimport/cinterop/cinterop.cpp
@@ -1,16 +1,21 @@
 #include "cinterop.hpp"
 
 void dlimport::push_cinterop(lua_State *L) {
+    const luaL_Reg structlib[] = {
+        {"newinfo", cinterop::create_struct_info},
+        {"getfield", cinterop::get_struct_field},
+        {"setfield", cinterop::set_struct_field},
+        {nullptr, nullptr}
+    };
     const luaL_Reg lib[] = {
-        {"new_function_binding", cinterop::new_function_binding},
-        //{"new_aggregate", cinterop::new_aggregate},
-        {"create_struct_info", cinterop::create_struct_info},
-        {"get_struct_field", cinterop::get_struct_field},
-        {"set_struct_field", cinterop::set_struct_field},
+        {"bindfunction", cinterop::new_function_binding},
         {nullptr, nullptr}
     };
     lua_newtable(L);
     luaL_register(L, nullptr, lib);
+    lua_newtable(L);
+    luaL_register(L, nullptr, structlib);
+    lua_setfield(L, -2, "struct");
     cinterop::push_c_types(L);
-    lua_setfield(L, -2, "native_type");
+    lua_setfield(L, -2, "native");
 }
